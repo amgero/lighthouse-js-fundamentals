a00fa30c54ebcdb7d6ab8ffa8bc61e389dc6aad3
Amgero <amgero@outlook.com>
2023-08-16 11:02:12 -0300

    edited code

--- challenge-whichSchool.js
+++ challenge-whichSchool.js
@@ -6,3 +6,39 @@ function whichSchool(age) {
   } else {
     return "Lighthouse Labs"; }
 }
+
+
+// extended work
+let bethanyAge = Math.round(Math.random() * 4) + 13
+let danyAge = Math.round(Math.random() * 12)
+let mariaAge = Math.round(Math.random() * 52) + 18
+
+let placeholder = false
+
+// Replace the placeholders with your own if conditions for each person.
+// Conditions for Bethany
+if (bethanyAge >= 13 && bethanyAge <= 16) {
+  console.log("Bethany should go to High School");
+} else if (bethanyAge > 16) {
+  console.log("Bethany should go to Lighthouse Labs");
+} else {
+  console.log("Bethany should go to Elementary School");
+}
+
+// Conditions for Dany
+if (danyAge >= 18) {
+  console.log("Dany should go to Lighthouse Labs");
+} else if (danyAge >= 6) {
+  console.log("Dany should go to High School");
+} else {
+  console.log("Dany should go to Elementary School");
+}
+
+// Conditions for Maria
+if (mariaAge >= 18) {
+  console.log("Maria should go to Lighthouse Labs");
+} else if (mariaAge >= 6) {
+  console.log("Maria should go to High School");
+} else {
+  console.log("Maria should go to Elementary School");
+}
